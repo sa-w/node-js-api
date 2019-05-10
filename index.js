81b219fcf7e0f2c944bb16f5886e0bd3896e0adb
unknown <steveakhwale83@gmail.com>
2019-05-10 14:37:18 +0300

    removing comment

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//var controllers = require('./api/controllers/controllers');
-
 var routes = require('./api/routes/routes');
 app.use(routes, function(req, res, next){
     next();
